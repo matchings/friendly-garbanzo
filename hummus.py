2e0a9082504b3c53b290f63af2cb26af2ebca7bd
matchings <marinag@alleninstitute.org>
2017-06-15 11:33:02 -0700

    changed to string input

--- hummus.py
+++ hummus.py
@@ -5,6 +5,5 @@ Created on Thu Jun 15 11:26:13 2017
 @author: marinag
 """
 
-def blend_garbanzo(beans):
-    hummus = beans*0.001
-    return hummus
+def blend_garbanzo(chickpea_string):
+    return '{}_hummus'.format(chickpea_string)
