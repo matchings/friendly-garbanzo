92e8d625a0cb5c56ba4c64d999aaa0192dea74c7
matchings <marinag@alleninstitute.org>
2017-06-15 11:45:22 -0700

    dont change if string includes hummus

--- hummus.py
+++ hummus.py
@@ -6,4 +6,7 @@ Created on Thu Jun 15 11:26:13 2017
 """
 
 def blend_garbanzo(chickpea_string):
-    return '{}_hummus'.format(chickpea_string)
+    if 'hummus' in chickpea_string: 
+        return chickpea_string
+    else:
+        return '{}_hummus'.format(chickpea_string)
